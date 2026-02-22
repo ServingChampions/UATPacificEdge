e656e8f3246d00409f455c6b03902fc2dce632ad
ServingChampions <140645995+ServingChampions@users.noreply.github.com>
2026-02-22 13:47:05 -0600

    Add files via upload

---
+++ assets/projects.js
@@ -0,0 +1,205 @@
+const myProjects = [
+  {
+    "title": "Anaheim Hills - Spanish Revival",
+    "summary": "Spanish inspired home nestled into the hills on an acre lot. New construction project with a remodeled hardscape to tie in the Spanish hacienda style.",
+    "specs": {
+      "Square Footage": "4,452sqft",
+      "Location": "Anaheim Hills, CA",
+      "Specs": "4 Bedrooms, 3 ½ Baths, Office, Detached Casita, Garage, Pool and Sports Court"
+    },
+    "designTeam": [
+      "Architect - Jason Richart",
+      "Landscape Designer - David Pederson"
+    ],
+    "images": [
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/0-web-or-mls-DSC01368.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/10-web-or-mls-DSC01119.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/13-web-or-mls-DSC01124.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/15-web-or-mls-DSC01353.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/16-web-or-mls-DSC01288.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/18-web-or-mls-DSC01202.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/20-web-or-mls-DSC01217.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/23-web-or-mls-DSC01242.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/25-web-or-mls-DSC01250.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/29-web-or-mls-DSC01264.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/30-web-or-mls-DSC01269.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/38-web-or-mls-DSC01318.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/5-web-or-mls-DSC01184%20copy.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/6-web-or-mls-DSC01186%20copy.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/anaheimhillsnew/9-web-or-mls-DSC01114.jpg?raw=true"
+    ]
+  },
+  {
+    "title": "Laguna Beach - Coastal Contemporary",
+    "summary": "Coastal Contemporary gem sitting on the bluffs of Laguna Beach. Designed by the renowned Horst Noppenberger. This house is the epitome of luxury coastal living, with exposed Steal Beams, floor to ceiling windows and doors showcasing unobstructed ocean views from all three levels.",
+    "specs": {
+      "Square Footage": "2,972 sqft including an ADU",
+      "Location": "Three Arch Bay, Laguna Beach, CA",
+      "Specs": "4 Bedrooms, 4 ½ Baths"
+    },
+    "designTeam": [
+      "Architect/Designer - Horst Noppenberger",
+      "Landscape Designer - M.D. Wilkes Design and Consulting"
+    ],
+    "images": [
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/1.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/7.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/8.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/2.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/5.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/4.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/3.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/10.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/12.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/14.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/15.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/16.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/17.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/18.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/21.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/22.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/23.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/6.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/coastalcontemp/9.jpg?raw=true"
+    ]
+  },
+  {
+    "title": "Newport Coast - Sophisticated Transitional",
+    "summary": "A transitional masterpiece hidden away in the highly regarded Newport Coast community. Teaming up with a fantastic design team to translate clean lines and seamless transitions from indoor to outdoor living.",
+    "specs": {
+      "Square Footage": "4,016 sqft house with a 600 sqft Garage",
+      "Location": "Newport Coast",
+      "Specs": "4 Bedrooms, 5 Baths, Office"
+    },
+    "designTeam": [
+      "Architect - Mitral Nehorai",
+      "Landscape Designer - Tim Fiskin",
+      "Designer - Donna Johnson"
+    ],
+    "images": [
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/0-web-or-mls-DSC00229.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/13-web-or-mls-DSC00320.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/14-web-or-mls-DSC00325.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/17-web-or-mls-DSC00106.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/18-web-or-mls-DSC00111.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/20-web-or-mls-DSC00126.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/23-web-or-mls-DSC00137.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/27-web-or-mls-DSC00160.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/28-web-or-mls-DSC00165.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/3-web-or-mls-DSC00245.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/30-web-or-mls-DSC00180.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/31-web-or-mls-DSC00185.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/35-web-or-mls-DSC00198.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/37-web-or-mls-DSC00218.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/4-web-or-mls-DSC00257.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/5-web-or-mls-DSC00267.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast/6-web-or-mls-DSC00272.jpeg?raw=true"
+    ]
+  },
+  {
+    "title": "Dana Point - Coastal Contemporary",
+    "summary": "A transitional full remodel project in the heart of Monarch Bay. Utilizing a luxurious floor plan to bring in indoor and outdoor living for those beautiful southern California days and nights. Boasting a 9′ Pivot entry door and 14′ tall ceilings for a grand entrance for a one of a kind build.",
+    "specs": {
+      "Square Footage": "3,731 sqft house with a 701 sqft Garage",
+      "Location": "Exclusive Gated Community in Monarch Bay",
+      "Specs": "3 Bedrooms and 4.5 Baths. Office, Flex Room and a 2-Car Garage with Golf Cart parking space."
+    },
+    "designTeam": [
+      "Architect - Jason Richart",
+      "Landscape Designer - David Pederson",
+      "Designer - Herring Design"
+    ],
+    "images": [
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h17.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h16.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h12.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h14.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h15.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h18.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h19.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h117.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h116.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h115.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h114.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h113.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h112.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h110.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/monarchbay/h11.jpeg?raw=true"
+    ]
+  },
+  {
+    "title": "Yorba Linda - Traditional Living",
+    "summary": "Installing a beautiful new hardscape/ landscape design to complement this new construction home. A traditional design including a BBQ with aluminum trellis, Sports Court, Pool and Spa with a beautiful full landscape design.",
+    "specs": {
+      "Square Footage": "",
+      "Location": "",
+      "Specs": ""
+    },
+    "designTeam": [
+      "Architect - ",
+      "Landscape Designer - "
+    ],
+    "images": [
+    "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorbavid2.mov?raw=true",
+     "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorba1.jpg?raw=true",
+    "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorba2.jpg?raw=true",
+    "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorba3.jpg?raw=true",
+    "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorba4.jpg?raw=true",
+    "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorba5.jpg?raw=true",
+    "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorba6.jpg?raw=true",
+    "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorba7.jpg?raw=true",
+    "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorba8.jpg?raw=true",
+    "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorba9.jpg?raw=true",
+    "https://github.com/ServingChampions/RegalPhotos/blob/main/yorbaupdated/yorba10.jpg?raw=true",
+    ]
+  },
+  {
+    "title": "Newport Coast - Transitional",
+    "summary": "Discover the epitome of luxury living in the heart of Newport Coast—a mesmerizing fusion of interior and exterior spaces. Immerse yourself in the allure of a newly crafted open floorplan seamlessly connected to an innovative outdoor design, elevating the art of entertainment and redefining modern living.",
+    "specs": {
+      "Scope": "Created an open floor plan design to integrate a seamless interior and exterior living space.",
+      "Location": "Newport Coast, CA"
+    },
+    "designTeam": [
+      "Architect - Jason Richart",
+      "Landscape Designer - Tim Fiskin",
+      "Interior and Hardscape Design - Pacific Edge Custom Homes"
+    ],
+    "images": [
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast2/h39.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast2/h38.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast2/h35.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast2/h37.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast2/h310.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast2/h36.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast2/h31.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast2/h32.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast2/h33.jpeg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/newportcoast2/h34.jpeg?raw=true"
+    ]
+  },
+
+{
+    "title": "Orange Park Acres - Spanish Inspired",
+    "summary": "This Spanish Revival residence blends classic California architecture with modern construction standards. The project features traditional stucco finishes, clay tile roofing, arched openings, and custom iron and wood detailing, executed with careful attention to authenticity and craftsmanship. Coordinated from pre-construction through completion, the result is a timeless home that honors Spanish Revival design while meeting today’s performance.",
+    "specs": {
+      "Location": "Orange Park Acres"
+    },
+    "designTeam": [
+      "Designer - Jennifer Herring"
+    ],
+    "images": [
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/opvid.mov?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/op1.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/op2.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/op3.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/op4.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/op5.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/op6.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/op7.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/op8.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/op9.jpg?raw=true",
+      "https://github.com/ServingChampions/RegalPhotos/blob/main/orangepark/op10.jpg?raw=true",
+    ]
+  },
+];
